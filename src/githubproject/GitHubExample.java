286e7aa978941fbf11427b72b1b2c69892ff32d1
José Robson <joserobsonsiqueira23@hotmail.com>
2022-06-09 13:19:57 -0300

    commit novo na branch cliente

--- src/githubproject/GitHubExample.java
+++ src/githubproject/GitHubExample.java
@@ -1,15 +1,24 @@
 package githubproject;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
+
+import githubproject.model.Cliente;
 
 public class GitHubExample {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Cliente cliente = new Cliente();
+		
 		System.out.println("Olá amigo.");
 		
 		System.out.println("github");
 
+		System.out.println("Informe seu nome: ");
+		String nome = sc.next();
+		cliente.setNome(nome);
+		
+		System.out.println(cliente);
 		
 	}
 
---
+++ src/githubproject/model/Cliente.java
@@ -0,0 +1,30 @@
+package githubproject.model;
+
+public class Cliente {
+
+	private String nome;
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nome=" + nome + "]";
+	}
+	
+	
+}
